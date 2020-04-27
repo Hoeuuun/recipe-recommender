6ce6e5a5e35eb20c3751fd2d176132b973a79490
Hoeun Sim <h.sim312@gmail.com>
2020-04-26 21:26:45 -0700

    Search (#6)
    * Very Basic search
    
    * Add Search component

---
+++ frontend/src/RestClient.js
@@ -0,0 +1,10 @@
+const serverAddress = 'http://localhost:5000'
+
+export function restRequest(endpoint) {
+    const url = `${serverAddress}/${endpoint}`;
+    return fetch(url, {
+        method: 'GET',
+    }).
+        then(response => response.json()).
+        catch(error => console.error(error));
+}
