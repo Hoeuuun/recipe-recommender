9758d81dc42684de85551f996b2ca54a2501afbe
Hoeun Sim <h.sim312@gmail.com>
2020-04-30 01:42:18 -0700

    Add card content and icons

--- frontend/src/components/SearchResultCard.js
+++ frontend/src/components/SearchResultCard.js
@@ -5,6 +5,16 @@ import CardHeader from "@material-ui/core/CardHeader";
 import CardMedia from "@material-ui/core/CardMedia";
 import makeStyles from "@material-ui/core/styles/makeStyles";
 import {red} from "@material-ui/core/colors";
+import Typography from "@material-ui/core/Typography";
+import CardContent from "@material-ui/core/CardContent";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import CardActions from "@material-ui/core/CardActions";
+import IconButton from "@material-ui/core/IconButton";
+import Collapse from "@material-ui/core/Collapse";
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+
+import clsx from 'clsx';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -31,15 +41,54 @@ const useStyles = makeStyles((theme) => ({
 
 export function SearchResultCard(props) {
     const classes = useStyles();
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
     return (
         <Card className={classes.root}>
             <CardHeader
                 title={props.title}
-                subheader=""
+                subheader={`Rating: ${props.rating}/100  |  Time: ${props.time} M`}
             />
-            <CardMedia className={classes.media} image={`${serverAddress}/images/userphotos/${props.image}`}
+            <CardMedia className={classes.media}
+                       image={`${serverAddress}/images/userphotos/${props.image}`}
             />
-
+            <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    {/*{props.url}*/}
+                    "TODO: Add ingredients list"
+                </Typography>
+            </CardContent>
+                <CardActions disableSpacing>
+                    <IconButton aria-label="add to favorites">
+                        <FavoriteIcon />
+                    </IconButton>
+                    <IconButton aria-label="share">
+                        <ShareIcon />
+                    </IconButton>
+                    <IconButton
+                        className={clsx(classes.expand, {
+                            [classes.expandOpen]: expanded,
+                        })}
+                        onClick={handleExpandClick}
+                        aria-expanded={expanded}
+                        aria-label="show more"
+                    >
+                        <ExpandMoreIcon />
+                    </IconButton>
+                </CardActions>
+                <Collapse in={expanded} timeout="auto" unmountOnExit>
+                    <CardContent>
+                        <Typography paragraph>Method:</Typography>
+                        <Typography paragraph>
+                            Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+                            minutes.
+                        </Typography>
+                    </CardContent>
+                </Collapse>
         </Card>
     );
 }
