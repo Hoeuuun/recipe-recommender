35d6e96f9679719eefb77feea6efea8ae3333120
Hoeun Sim <h.sim312@gmail.com>
2020-05-17 21:44:25 -0700

    Add two helper functions, get_recipe_steps and search_title

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from flask_sqlalchemy import Model
-from backend.model import Recipe, db, RecipeStep, Ingredient
+from backend.model import Recipe, db, RecipeStep, Ingredient, RecipeIngredient
 from typing import List
 
 
@@ -7,6 +7,7 @@ def insert_recipe(title, rating, review, time, description) -> None:
     """ Adds a new recipe """
     steps = [RecipeStep(step_num=1, instruction='Mix everything together'),
              RecipeStep(step_num=2, instruction='Pour batter and cook.')]
+
     recipe = Recipe(title=title, rating=rating, review=review, time=time, description=description, steps=steps)
 
     db.session.add(recipe)
@@ -35,10 +36,33 @@ def create_ingredient(name) -> Ingredient:
     return ing
 
 
+def get_recipe_steps(recipe_id: int) -> List[tuple]:
+    """ Returns a list of recipe steps (number and methods/directions) by id"""
+    steps = []
+
+    for step in RecipeStep.query.filter(RecipeStep.recipe_id == recipe_id):
+        steps.append((step.step_num, step.instruction))
+        # steps.append(step.__dict__)
+
+    print(steps)
+    return steps
+
+
+def search_title(search_key: str) -> List[Recipe]:
+    """ Returns a list of Recipes containing the search key in title/name """
+    recipes = Recipe.query.filter(Recipe.title.like(f'%{search_key}%')).all()
+    print('**', recipes)
+
+    for r in recipes:
+        print('id:', r.id, 'title:', r.title, 'Ingredients:', r.ingredients)
+
+    return recipes
+
+
 def main():
     db.drop_all()
     db.create_all()
-    insert_recipe('Tasty Pancakes', 4, 100, 15, 'These pancakes are yummy')
+    insert_recipe('Tasty Pancakes fruit', 4, 100, 15, 'These pancakes are yummy')
     insert_recipe('Fruit Salad', 3, 300, 5, 'Perfecto!')
     # delete_recipe(1)
     recipe = find_recipe(1)
@@ -46,6 +70,9 @@ def main():
     recipe.ingredients = [ing]
     ing = create_ingredient('Grapes')
     recipe.ingredients.append(ing)
+
+    get_recipe_steps(1)
+    search_title('fruit')
 
     db.session.commit()
 
