c3949eb455a24b7670298c881728a94a01c0a36d
Hoeun Sim <h.sim312@gmail.com>
2020-09-01 21:12:24 -0700

    Use Sqlalchemy for search

--- backend/model.py
+++ backend/model.py
@@ -1,19 +1,21 @@
+from typing import Dict
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 from backend.app import app
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipes.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 
 
 class Recipe(db.Model):
-    __tablename__ = 'recipes'
+    __tablename__ = 'Recipe'
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
     rating = db.Column(db.Integer)
-    review = db.Column(db.Integer)
+    review_count = db.Column(db.Integer)
     time = db.Column(db.Integer)
     image = db.Column(db.String)
     url = db.Column(db.String)
@@ -21,7 +23,7 @@ class Recipe(db.Model):
 
     steps = db.relationship('RecipeStep', back_populates='recipe')
     ingredients = db.relationship('Ingredient',
-                                  secondary='recipe_ingredients')
+                                  secondary='RecipeIngredients')
 
     def __init__(self, title, rating, review, time, image, url, description, steps, ingredients):
         self.title = title
@@ -34,17 +36,35 @@ class Recipe(db.Model):
         self.steps = steps
         self.ingredients = ingredients
 
+    def to_dict(self) -> Dict:
+        return {
+            'id': self.id,
+            'title': self.title,
+            'rating': self.rating,
+            'review_count': self.review_count,
+            'time': self.time,
+            'image': self.image,
+            'description': self.description,
+            'ingredients': [ingredient.name for ingredient in self.ingredients],
+            'directions': [step.instruction for step in self.steps],
+        }
 
 class Ingredient(db.Model):
-    __tablename__ = 'ingredients'
+    __tablename__ = 'Ingredient'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
 
+    def to_dict(self) -> Dict:
+        return {
+            'id': self.id,
+            'name': self.name
+        }
+
 
 class RecipeIngredient(db.Model):
-    __tablename__ = 'recipe_ingredients'
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'), primary_key=True)
-    ingredient_id = db.Column(db.Integer, db.ForeignKey('ingredients.id'), primary_key=True)
+    __tablename__ = 'RecipeIngredients'
+    recipeId = db.Column(db.Integer, db.ForeignKey('Recipe.id'), primary_key=True)
+    ingredientId = db.Column(db.Integer, db.ForeignKey('Ingredient.id'), primary_key=True)
     quantity = db.Column(db.String)
     value = db.Column(db.Float)
     unit = db.Column(db.String)
@@ -53,9 +73,15 @@ class RecipeIngredient(db.Model):
 
 
 class RecipeStep(db.Model):
-    __tablename__ = 'recipe_steps'
-    recipe_id = db.Column(db.Integer, db.ForeignKey('recipes.id'), primary_key=True)
-    step_num = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'RecipeSteps'
+    recipeId = db.Column(db.Integer, db.ForeignKey('Recipe.id'), primary_key=True)
+    stepNumber = db.Column(db.Integer, primary_key=True)
     instruction = db.Column(db.String, nullable=False)
 
     recipe = db.relationship('Recipe', back_populates='steps')
+
+    def to_dict(self) -> Dict:
+        return {
+            'stepNumber': self.stepNumber,
+            'instruction': self.instruction,
+        }
--- backend/server.py
+++ backend/server.py
@@ -4,6 +4,9 @@ import pickle
 import sqlite3
 
 from flask import send_from_directory, request, jsonify, abort
+from sqlalchemy import and_, desc, asc, or_
+
+from backend.model import db, Recipe, RecipeIngredient, Ingredient, RecipeStep
 from backend.sqlite_helper import search_by_ingredients
 
 from backend.app import app
@@ -58,6 +61,35 @@ def search():
 
     review_count = request.args.get('review', default=None, type=str)
 
+    ilike_query = '%'.join(q.split(','))
+    ilike_query = f'%{ilike_query}%'
+    recipes_query = db.session.query(Recipe).filter(
+        or_(
+            Recipe.description.ilike(ilike_query),
+            Recipe.title.ilike(ilike_query),
+            Recipe.ingredients.any(Ingredient.name.ilike(ilike_query)),
+            # Recipe.steps.any(RecipeStep.instruction.ilike(ilike_query))
+        ))
+
+    if minTime is not None and maxTime is not None:
+        recipes_query = recipes_query.filter(and_(
+            Recipe.time >= minTime,
+            Recipe.time <= maxTime
+        ))
+    if review_count is not None:
+        fn = desc if review_count == 'DESC' else asc
+        recipes_query = recipes_query.order_by(fn(Recipe.review_count))
+    if rating is not None:
+        fn = desc if rating == 'DESC' else asc
+        recipes_query = recipes_query.order_by(fn(Recipe.rating))
+
+    recipes_query = recipes_query.limit(50)
+
+    recipes = recipes_query.all()
+
+
+
+    """
     recipes = search_by_ingredients(connection, q.split(","),
                                     ingredient_index=INGREDIENT_INDEX)
 
@@ -72,8 +104,9 @@ def search():
     if rating is not None:
         print(f"Sorting by {rating}")
         recipes = sorted(recipes, key=lambda recipe: recipe['rating'], reverse=rating == 'DESC')
+    """
 
-    return jsonify({'total': len(recipes), 'data': recipes[0:50]})
+    return jsonify({'total': len(recipes), 'data': [recipe.to_dict() for recipe in recipes]})
 
 
 if __name__ == "__main__":
