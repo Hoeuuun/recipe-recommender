fbff96a85cc3304f5d6ece1786528d7c111a4533
Hoeun Sim <h.sim312@gmail.com>
2020-11-02 16:48:16 -0800

    Testing (#25)
    * First tests for search endpoint
    
    * Add more tests for text search
    
    * Add tests for filtering by reviews
    
    * Add tests for rating and time filters

--- backend/tests/server_test.py
+++ backend/tests/server_test.py
@@ -26,6 +26,12 @@ def test_search_in_recipe_title(app, client):
 def test_search_in_recipe_description(app, client):
     # Given: An egg salad exists in the database with egg in
     # description but not title
+    with app.app_context():
+        egg_salad = Recipe(
+            title='Yummy Salad Recipe',
+            description='It has eggs')
+        db.session.add(egg_salad)
+        db.session.commit()
 
     # When: We search for egg
     response = client.get('/search?q=egg')
@@ -75,14 +81,13 @@ def test_search_no_results(app, client):
     data = response.get_json()
     assert data['total'] == 0
 
-
-def assert_sorted(list_:List, key:str, reverse:bool):
+# tests for filters
+def assert_sorted(list_: List, key: str, reverse: bool):
     sorted_list = sorted(list_, key=lambda x: x[key], reverse=reverse)
     assert sorted_list == list_
 
 
-# TODO: tests for filters
-def test_search_by_review_count(app, client):
+def test_filter_by_review_count(app, client):
     # Given: More than one egg recipe with different
     # review counts
     with app.app_context():
@@ -102,8 +107,9 @@ def test_search_by_review_count(app, client):
         db.session.add(egg_recipe2)
         db.session.commit()
 
-        recipes = db.session.query(Recipe).all()
-        print(recipes)
+        # recipes = db.session.query(Recipe).all()
+        # print(recipes)
+        db.session.query(Recipe).all()
 
     # When: When we filter egg results by "Highest" reviews
     response = client.get('/search?q=egg&review=DESC')
@@ -128,3 +134,104 @@ def test_search_by_review_count(app, client):
     assert_sorted(data['data'], 'review_count', False)
 
 
+def test_filter_by_rating(app, client):
+    # Given: More than one recipe with different ratings
+    with app.app_context():
+        egg_recipe1 = Recipe(
+            title='Egg Salad',
+            description='Eggcellent!',
+            rating=2,
+            ingredients=[Ingredient(name='1 teaspoon of brown sugar')],
+            review_count=3
+        )
+        egg_recipe2 = Recipe(
+            title='Egg Summer Salad',
+            description='Egg-licious!',
+            rating=1,
+            ingredients=[Ingredient(name='2 teaspoons of brown sugar')],
+            review_count=100
+        )
+        db.session.add(egg_recipe1)
+        db.session.add(egg_recipe2)
+        db.session.commit()
+
+        # recipes = db.session.query(Recipe).all()
+        # print(recipes)
+        db.session.query(Recipe).all()
+
+    # When: We filter the recipe results by "Highest" rating
+    response = client.get('/search?q=egg&rating=DESC')
+    assert response.status_code == HTTPStatus.OK
+
+    # Then: We should get back the results in descending order,
+    # with the highest-rated recipe displayed first
+    data = response.get_json()
+    # print(data)
+    assert data['total'] == 2
+
+    assert_sorted(data['data'], 'rating', True)
+
+    # When: We filter the recipe results by "Lowest" rating
+    response = client.get('/search?q=egg&rating=ASC')
+    assert response.status_code == HTTPStatus.OK
+
+    # Then We should get the back the results in ascending order,
+    # with the lowest-rated recipe displayed first
+    data = response.get_json()
+    assert data['total'] == 2
+
+    assert_sorted(data['data'], 'rating', False)
+
+
+def test_filter_by_time(app, client):
+    # Given: More than one recipe with different cooking times
+    with app.app_context():
+        egg_recipe1 = Recipe(
+            title='Egg 1',
+            time=15
+        )
+        egg_recipe2 = Recipe(
+            title='Egg 2',
+            time=31
+        )
+        egg_recipe3 = Recipe(
+            title='Egg 3',
+            time=60
+        )
+        egg_recipe4 = Recipe(
+            title='Egg 4',
+            time=75
+        )
+        db.session.add(egg_recipe1)
+        db.session.add(egg_recipe2)
+        db.session.add(egg_recipe3)
+        db.session.add(egg_recipe4)
+        db.session.commit()
+
+        # recipes = db.session.query(Recipe).all()
+        # print(recipes)
+        db.session.query(Recipe).all()
+
+    # When: We filter the recipe results by "< 15" option
+    response = client.get('/search?q=egg&minTime=0&maxTime=15')
+    assert response.status_code == HTTPStatus.OK
+
+    # Then: We should get back only recipes with under 16 mins cooking time
+    data = response.get_json()
+    assert data['total'] == 1
+
+    # When: We filter by "< 30"
+    response = client.get('/search?q=egg&minTime=0&maxTime=30')
+    assert response.status_code == HTTPStatus.OK
+
+    # Then: We should get back only recipes with under 31 mins cooking time
+    data = response.get_json()
+    assert data['total'] == 1
+
+    # When: We filter by "< 60"
+    response = client.get('/search?q=egg&minTime=0&maxTime=60')
+    assert response.status_code == HTTPStatus.OK
+
+    # Then We should get back only recipes with 1 hour or less cooking time
+    data = response.get_json()
+    assert data['total'] == 3
