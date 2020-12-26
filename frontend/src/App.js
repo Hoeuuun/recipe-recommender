ff50c6b57f29f2c434cc3a2fc82ee7fc7c7dac4d
Hoeun Sim <h.sim312@gmail.com>
2020-12-26 00:41:58 -0800

    Set up Auth0 login feature (#31)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,10 +4,19 @@ import {Header} from "./components/Header";
 import {Search} from "./components/Search";
 import {Contact} from "./components/Contact";
 import {Footer} from "./components/Footer";
+import {LoginButton} from "./components/LoginButton";
+import {Auth0Provider} from "@auth0/auth0-react";
 
 export default function App() {
   return (
     <div>
+        <Auth0Provider
+            domain="dev-degk2yc5.us.auth0.com"
+            clientId="tRrtNJsbjsTHkJ7vzZLZiFs13jEBhFdA"
+            redirectUri={window.location.origin}
+        >
+            <LoginButton/>
+        </Auth0Provider>
         <NavBar/>
         <Header/>
         <Search/>
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,9 +7,20 @@ import * as serviceWorker from './serviceWorker';
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
+import { Auth0Provider } from '@auth0/auth0-react';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Auth0Provider
+        domain="dev-degk2yc5.us.auth0.com"
+        clientId="tRrtNJsbjsTHkJ7vzZLZiFs13jEBhFdA"
+        redirectUri={window.location.origin}
+    >
+        <App />
+    </Auth0Provider>,
+    document.getElementById('root')
+);
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
